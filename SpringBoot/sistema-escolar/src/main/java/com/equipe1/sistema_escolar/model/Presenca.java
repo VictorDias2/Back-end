2e832e035a4ab41638e8004388aeb2a6e164f8e8
VictorDias2 <victorrdias2015@gmail.com>
2024-11-28 12:52:23 -0300

    .

---
+++ SpringBoot/sistema-escolar/src/main/java/com/equipe1/sistema_escolar/model/Presenca.java
@@ -0,0 +1,35 @@
+package com.equipe1.sistema_escolar.model;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_presenca")
+public class Presenca {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pk_id_presenca")
+    private Long Id;
+
+    @Column (name = "data_presenca")
+    private Date datapresenca;
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Date getDatapresenca() {
+        return datapresenca;
+    }
+
+    public void setDatapresenca(Date datapresenca) {
+        this.datapresenca = datapresenca;
+    }
+}
---
+++ SpringBoot/sistema-escolar/src/main/java/com/equipe1/sistema_escolar/model/Sala.java
@@ -0,0 +1,45 @@
+package com.equipe1.sistema_escolar.model;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+
+@Entity
+@Table(name = "tb_sala")
+public class Sala {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pk_id_sala")
+    private Long Id;
+
+    @Column (name = "nome_sala")
+    private String nome;
+
+    @Column (name = "capacidade")
+    private int capacidade;
+
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+}
